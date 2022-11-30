2f80efdb53fff465f3ecbb0248fafdf464a3f647
Lê Đạt coder <93327481+ledatcoder@users.noreply.github.com>
2022-12-01 04:36:14 +0700

    API Paging, Filtering, Sorting & Searching

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,6 +13,24 @@ namespace Infrastructure.Data
         public DbSet<Product> Products {set;get;}
         public DbSet<ProductBrand> ProductBrands {set;get;}
         public DbSet<ProductType> ProductTypes {set;get;}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+            {
+                 foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+                {
+                    var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType == typeof(decimal));
+
+                    foreach (var property in properties)
+                    {
+                        modelBuilder.Entity(entityType.Name).Property(property.Name).HasConversion<double>();
+                    }
+                }
+            }
+        }
 
     }
 }
